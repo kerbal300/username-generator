04ed83b0d20efc60ab57222fd105fd5b656e4e8e
Kerbal_YT <58401953+kerbal300@users.noreply.github.com>
2023-03-22 07:09:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,29 +11,29 @@ function makeid(length) {
 }
 
 document.getElementById('4').onclick = function() {
-   document.getElementById('gen-box').innerHTML = makeid(4)
-};
+   document.getElementById('gen-box').innerHTML = makeid(4);
+}
 
 document.getElementById('5').onclick = function() {
-   document.getElementById('gen-box').innerHTML = makeid(5)
-};
+   document.getElementById('gen-box').innerHTML = makeid(5);
+}
 
 document.getElementById('6').onclick = function() {
-   document.getElementById('gen-box').innerHTML = makeid(6)
-};
+   document.getElementById('gen-box').innerHTML = makeid(6);
+}
 
 document.getElementById('7').onclick = function() {
-   document.getElementById('gen-box').innerHTML = makeid(7)
-};
+   document.getElementById('gen-box').innerHTML = makeid(7);
+}
 
 document.getElementById('8').onclick = function() {
-   document.getElementById('gen-box').innerHTML = makeid(8)
-};
+   document.getElementById('gen-box').innerHTML = makeid(8);
+}
 
 document.getElementById('9').onclick = function() {
-   document.getElementById('gen-box').innerHTML = makeid(9)
-};
+   document.getElementById('gen-box').innerHTML = makeid(9);
+}
 
 document.getElementById('10').onclick = function() {
-   document.getElementById('gen-box').innerHTML = makeid(10)
+   document.getElementById('gen-box').innerHTML = makeid(10);
 };
